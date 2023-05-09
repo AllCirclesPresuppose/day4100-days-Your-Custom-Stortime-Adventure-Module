f8e52135b60f4446361a8cc1fb398f8f4be9c54d
AllCirclesPresuppose <114363778+AllCirclesPresuppose@users.noreply.github.com>
2023-05-09 19:51:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+print("Your Custom Storytime Adventure Module!")
+print()
+print(
+    "\033[33m*high pitched robot voice*\033[0mHello! I'm \033[32mS.A.M.\033[0m! Your Custom\033[32m",
+    "Storytime Advenuture Module helper\033[0m! Together, we're going to come up with an epic tale! Let's get started!"
+)
+name = input(
+    "First, we'll need your \033[34mname\033[0m. What should I call you? ")
+like = input(
+    "Great, thank you so much! Next, tell me about an \033[36mobject\033[0m you like! "
+)
+hate = input("Perfect! Now I need an a thing you \033[31mhate\033[0m! ")
+friends = input(
+    "Wonderful! for this last part, what is a \033[33mfriends\033[0m name? ")
+print()
+print("Thank you so much ", name,
+      "! I'm formulating your story below, it's gunna be grrrreat!")
+print(
+    "\033[33m*movie trailer voice*\033[0m An epic tale, brought to you by \033[32mS.A.M.\033[0m and \033[34m",
+    name, "\033[0m the likes of which you've never heard before...")
+print()
+print()
+location = input(
+    "(oh shoot, we almost forgot, where does this epic story take place?) ")
+print()
+print()
+print(
+    "In the far off lands of ", location,
+    "a great and mighty evil, the \033[31munstoppable ", hate,
+    "\033[0mabides. The ancients speak of a great power, something that can be used to contain this mighty evil, called \033[36m",
+    like, "\033[0m.")
+print()
+print(
+    "In this land lived \033[33m", friends,
+    "\033[0m, a close friend to \033[34m", name,
+    "\033[0m. But there was a problem! \033[33m", friends,
+    "\033[0m had gone seeking power, and became corrupted by the \033[31mterrible, mighty influence of",
+    hate, "\033[0m!")
+print()
+print(
+    "\033[34m", name, "\033[0m set off with their \033[36mtrusty ", like,
+    "\033[0m, knowing that only the true powers held within \033[36m", like,
+    "\033[0m and, perhaps, the \033[32mtrue powers of friendship\033[0m, could break the curse placed upon\033[33m",
+    friends, "\033[0m.")
+print()
+print(
+    "\033[31mA mighty duel ensued in the lands of ", location,
+    " the likes that were never seen before, and have never been seen since.\033[0m \033[33m",
+    friends, "\033[0m, being corrupted by the \033[31mterrible power of ",
+    hate, "\033[0m, was a terrible sight to behold. Valiantly, \033[34m", name,
+    "\033[0m weilded \033[36m", like,
+    "\033[0m and ultimatly relinqueshed \033[31m", hate,
+    "s\033[0m control over \033[33m", friends, "\033[0m!")
+print()
+print("With one final push of power coming from \033[36m", like,
+      "\033[0m(and from friendship) the \033[31mgreat evil ", hate,
+      "\033[0m was trapped away once again.")
+print("The end!")
+print()
+print()
+print()
+print("\033[35m...Or is it!?!?!")
